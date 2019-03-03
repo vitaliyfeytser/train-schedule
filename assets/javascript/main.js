363b48d3325884f8dfee2bf8732e48c55da54da8
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-03-02 21:15:11 -0800

    firebase works well with the remove button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,26 +1,19 @@
-// CONNECT FIREBASE
+// CONNECT FIREBASE //////////
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDlrrFn3DljUQSE9N69j2Olgec_5_erguQ",
+    authDomain: "train-schedule-d6eac.firebaseapp.com",
+    databaseURL: "https://train-schedule-d6eac.firebaseio.com",
+    projectId: "train-schedule-d6eac",
+    storageBucket: "train-schedule-d6eac.appspot.com",
+    messagingSenderId: "653281352895"
+};
+firebase.initializeApp(config);
 
-// local object for handling saved train info
-var trainObject = [
-    {
-        trainName: 'Midnight Express',
-        destination: 'San Francisco',
-        firstTrainTime: '3:45',
-        frequency: '90'
-        // nextArrival: 'nextArrival()',
-        // minutesAway: ''
-    },
-    {
-        trainName: 'Lazy Noon',
-        destination: 'San Diego',
-        firstTrainTime: '1:55',
-        frequency: '380'
-        // nextArrival: 'nextArrival()',
-        // minutesAway: ''
-    }
-]
-console.log('trainObject at start: ', trainObject);
+var database = firebase.database();
+
+/////////////////////////////
 
 // variables for handling saved train info
 var trainName = '';
@@ -34,142 +27,16 @@ var minutesAway = '';
 
 // get current time with javascipt or moment-js
 var now = Date.now();
-// var now = moment();
 console.log('now: ', now);
-// var now2 = moment().hours();
-
+// optional current time
 var now2 = moment().valueOf();
 console.log('now2: ', now2);
 
-
-// Sample code to use momentJS to default to 'today' with hours and minutes specified
-// moment({hour: 5, minute: 10});  // today, 5:10.00.000
-
-// $(document).ready(function () {
-
-
-function firstRun() {
-    // empty the train table before appending with new info
-    $('#train-table').empty();
-
-    for (var i = 0; i < trainObject.length; i++) {
-
-        trainName = trainObject.trainName;
-        destination = trainObject.destination;
-        firstTrainTime = trainObject.firstTrainTime;
-        frequency = trainObject.frequency;
-
-
-        // take first train time .. of the day
-        // take current time and subtract first time - product is in milliseconds
-        var firstCurrentDiff = (moment().diff(moment(trainObject[i].firstTrainTime, 'LT')));
-        console.log('firstCurrentDiff: ', firstCurrentDiff);
-
-        // verifying that this value is in milliseconds
-        var firstCurrentDiff_divided = firstCurrentDiff / 1000 / 60 / 60;
-        console.log('firstCurrentDiff_divided: ', firstCurrentDiff_divided);
-
-        // set frequency to milliseconds
-        var frequencyMilSec = trainObject[i].frequency * 60 * 1000;
-        console.log('frequencyMilSec: ', frequencyMilSec);
-
-        // find number of 'frequency' intervals that already occured today
-        var numOfTrainsToday = '';
-        if (firstCurrentDiff / frequencyMilSec <= 0) {
-            numOfTrainsToday = 0;
-        } else {
-            numOfTrainsToday = firstCurrentDiff / frequencyMilSec;
-            console.log('numOfTrainsToday: ', numOfTrainsToday);
-        }
-
-        // round up to the next 'frequency' interval
-        var nextTrain = Math.ceil(numOfTrainsToday);
-        console.log('nextTrain: ', nextTrain);
-
-        // set first train time to milliseconds
-        var firstTrainTimeMilSec = moment(trainObject[i].firstTrainTime, 'LT').valueOf();
-        console.log('firstTrainTimeMilSec: ', firstTrainTimeMilSec);
-
-        // get total time that will pass at next occurrence of 'frequency' in milliseconds
-        var nextTrainIntervalMilSec = nextTrain * frequencyMilSec;
-        console.log('nextTrainIntervalMilSec: ', nextTrainIntervalMilSec);
-
-        // add next train interval to first train time to get a unix millisecond time of next train
-        var nextTrainTimeMilSec = Number(nextTrainIntervalMilSec) + Number(firstTrainTimeMilSec);
-        console.log('nextTrainTimeMilSec: ', nextTrainTimeMilSec);
-        console.log('now2---------------: ', now2);
-
-        // convert unix timestamp to readable time today
-        var readableNextTrainTime = moment(nextTrainTimeMilSec).format("H:mm");
-        console.log('readableNextTrainTime: ', readableNextTrainTime);
-
-        // set next arrival time to variable that creates table rows
-        nextArrival = readableNextTrainTime;
-        console.log('nextArrival: ', nextArrival);
-
-        // find the difference between now and the next train
-        var minutesAwayMilSec = Number(nextTrainTimeMilSec) - Number(moment().valueOf());
-        var minutesAwayMilSec2 = Number(nextTrainTimeMilSec) - now2;
-        console.log('minutesAwayMilSec: ', minutesAwayMilSec);
-        console.log('minutesAwayMilSec2: ', minutesAwayMilSec2);
-
-        // set minutes away in readable format to the variable that is used to create table rows
-
-        minutesAway = Math.ceil(minutesAwayMilSec / 1000 / 60);
-        // minutesAway = moment(minutesAwayMilSec).format("Y M d H:mm");
-        // minutesAway2 = moment(minutesAwayMilSec).format("H:mm");
-        console.log('minutesAway: ', minutesAway);
-        // console.log('minutesAway2: ', minutesAway2);
-
-
-        // divide difference by frequency
-        // var frequencySum = firstCurrentDiff / moment(trainObject[i].frequency, 'x');
-        // console.log('frequencySum: ', frequencySum);
-        // // get result with remainder
-        // var remainder = firstCurrentDiff % trainObject[i].frequency;
-        // console.log('remainder: ', remainder);
-        // var noRemainder = frequencySum - remainder;
-        // console.log('noRemainder: ', noRemainder);
-        // multiply frequency by whole numbers of result and add 1-frequency to get next train time
-
-        function tableRowCreator() {
-            $('<tr>').append(
-                $('<th>', {
-                    scope: 'row',
-                    text: trainObject[i].trainName
-                }),
-                $('<td>', {
-                    text: trainObject[i].destination
-                }),
-                $('<td>', {
-                    text: trainObject[i].firstTrainTime
-                }),
-                $('<td>', {
-                    text: trainObject[i].frequency
-                }),
-                $('<td>', {
-                    text: nextArrival
-                }),
-                $('<td>', {
-                    text: minutesAway
-                }),
-            ).appendTo('#train-table');
-        }
-        tableRowCreator();
-
-    }
-
-}
-firstRun();
-
-function minutesAway() {
-    // multiply frequency by remainder and subtract from frequency to get minutes-away time
-
-}
-
+// on-click event listener for new-train submit button
 $('#submit-button').click(function () {
     event.preventDefault();
 
+    // grab input from new-train entry fields
     trainName = $('#train-name').val();
     destination = $('#destination').val();
     firstTrainTime = $('#first-train-time').val();
@@ -180,20 +47,154 @@ $('#submit-button').click(function () {
     console.log('newFirstTrainTime: ', firstTrainTime);
     console.log('newFrequency: ', frequency);
 
-    trainObject.push({
+    // create a train-object for push to Firebase database
+    trainObject = {
         trainName: trainName,
         destination: destination,
         firstTrainTime: firstTrainTime,
-        frequency: frequency,
-        nextArrival: 'funciton nextArrival()',
-        minutesAway: 'function minutesAway()'
-    })
+        frequency: frequency
+    }
 
-    console.log('trainObject: ', trainObject);
+    // Uploads train data to the database
+    database.ref().push(trainObject);
 
-    firstRun();
+    // Clear the new-train entry fields on-click of submit button
+    $('#train-name').val('');
+    $('#destination').val('');
+    $('#first-train-time').val('');
+    $('#frequency').val('');
 });
 
+var trainCount = 0;
 
+// Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
+    console.log('childSnapshot.val(): ', childSnapshot.val());
 
-// });
+    // Store everything into a variable.
+    trainName = childSnapshot.val().trainName;
+    destination = childSnapshot.val().destination;
+    firstTrainTime = childSnapshot.val().firstTrainTime;
+    frequency = childSnapshot.val().frequency;
+
+    // Train Info
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrainTime);
+    console.log(frequency);
+
+    // take first train time .. of the day
+    // take current time and subtract first time - product is in milliseconds
+    var firstCurrentDiff = (moment().diff(moment(firstTrainTime, 'LT')));
+    console.log('firstCurrentDiff: ', firstCurrentDiff);
+
+    // verifying that this value is in milliseconds
+    var firstCurrentDiff_divided = firstCurrentDiff / 1000 / 60 / 60;
+    console.log('firstCurrentDiff_divided: ', firstCurrentDiff_divided);
+
+    // set frequency to milliseconds
+    var frequencyMilSec = frequency * 60 * 1000;
+    console.log('frequencyMilSec: ', frequencyMilSec);
+
+    // find number of 'frequency' intervals that already occured today
+    var numOfTrainsToday = '';
+    if (firstCurrentDiff / frequencyMilSec <= 0) {
+        numOfTrainsToday = 0;
+    } else {
+        numOfTrainsToday = firstCurrentDiff / frequencyMilSec;
+        console.log('numOfTrainsToday: ', numOfTrainsToday);
+    }
+
+    // round up to the next 'frequency' interval
+    var nextTrain = Math.ceil(numOfTrainsToday);
+    console.log('nextTrain: ', nextTrain);
+
+    // set first train time to milliseconds
+    var firstTrainTimeMilSec = moment(firstTrainTime, 'LT').valueOf();
+    console.log('firstTrainTimeMilSec: ', firstTrainTimeMilSec);
+
+    // get total time that will pass at next occurrence of 'frequency' in milliseconds
+    var nextTrainIntervalMilSec = nextTrain * frequencyMilSec;
+    console.log('nextTrainIntervalMilSec: ', nextTrainIntervalMilSec);
+
+    // add next train interval to first train time to get a unix millisecond time of next train
+    var nextTrainTimeMilSec = Number(nextTrainIntervalMilSec) + Number(firstTrainTimeMilSec);
+    console.log('nextTrainTimeMilSec: ', nextTrainTimeMilSec);
+    console.log('now2---------------: ', now2);
+
+    // convert unix timestamp to readable time today
+    var readableNextTrainTime = moment(nextTrainTimeMilSec).format("H:mm");
+    console.log('readableNextTrainTime: ', readableNextTrainTime);
+
+    // set next arrival time to variable that creates table rows
+    nextArrival = readableNextTrainTime;
+    console.log('nextArrival: ', nextArrival);
+
+    // find the difference between now and the next train
+    var minutesAwayMilSec = Number(nextTrainTimeMilSec) - Number(moment().valueOf());
+    console.log('minutesAwayMilSec: ', minutesAwayMilSec);
+
+    // set minutes away in readable format to the variable that is used to create table rows
+    minutesAway = Math.ceil(minutesAwayMilSec / 1000 / 60);
+    console.log('minutesAway: ', minutesAway);
+
+    // Creates the new train schedule row
+    function tableRowCreator() {
+        $('<tr>', {
+            // unique id setter for each <tr> html row elemnent
+            id: 'train-num-' + trainCount
+        }).append(
+            $('<th>', {
+                scope: 'row',
+                text: trainName,
+                id: 'train-name-' + trainCount
+            }),
+            $('<td>', {
+                text: destination,
+                id: 'destination-' + trainCount
+            }),
+            $('<td>', {
+                text: firstTrainTime,
+                id: 'first-train-time-' + trainCount
+            }),
+            $('<td>', {
+                text: frequency,
+                id: 'frequency-' + trainCount
+            }),
+            $('<td>', {
+                text: nextArrival
+            }),
+            $('<td>', {
+                text: minutesAway
+            }),
+            $('<td>').append(
+                $('<button>', {
+                    type: "button",
+                    class: "btn btn-secondary btn-sm remove-button",
+                    text: 'Remove',
+                    // train-schedule row marker that is same as train-mun-* in the <tr> html element 
+                    // that allows deletetion of correct html element upon click of the Remove button
+                    'data-train': trainCount,
+                    // Firebase key for addressing this object in the firebase database
+                    'data-key': childSnapshot.key
+                })
+            )
+        ).appendTo('#train-table');
+    }
+    tableRowCreator();
+
+    trainCount++
+});
+
+// event listener for the train schedule row Remove button
+$(document.body).on("click", ".remove-button", function () {
+
+    // select and remove the matching info in Firebase database
+    var ref = database.ref("/" + $(this).attr("data-key"));
+    console.log(ref);
+    ref.remove();
+
+    // Select and Remove the specific <tr> element that holds the specific train info.
+    $("#train-num-" + $(this).attr("data-train")).remove();
+
+});
